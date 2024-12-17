1fc50b3cd68f59346077e11faf43f38cb03b2f8f
Harsh <harsh.prajapati@techifysolutions.com>
2024-12-17 22:16:58 +0530

    Initial Commit

---
+++ friendListDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  friendListDemo
+//
+//  Created by Techify on 17/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
